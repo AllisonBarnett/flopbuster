9ec87594084fd13d846b80d25537c951af683850
Allison <abarnett17@gmail.com>
2021-07-18 09:37:44 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'flopbuster';
+
+  flops = {
+    hobbit:
+      'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/original/products/86974/86527/the_hobbit_the_desolution_of_smaug__final_poster_buy_original_movie_posters_at_starstills__35272__21743.1394515789.jpg?c=2',
+    newMoon:
+      'https://cdn.shopify.com/s/files/1/1416/8662/products/twilightnewmoon_2009_original_film_art_b4de3022-d6fa-43f4-815a-c2cc4df164f1_1200x.jpg?v=1618949594',
+    velocipastor:
+      'https://m.media-amazon.com/images/M/MV5BZTJiZjFkNmYtNmU3My00MmE1LWI4YWEtNWFkZDJiOTgwMTNkXkEyXkFqcGdeQXVyMTg0MTI3Mg@@._V1_.jpg',
+    cryWilderness:
+      'https://cdn.cinematerial.com/p/297x/knnahuxr/cry-wilderness-movie-poster-md.jpg?v=1456385813',
+  };
 }
