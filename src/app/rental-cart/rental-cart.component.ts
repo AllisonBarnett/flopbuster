27738fd2717708b0ddf5b8c44a676956fe9c28d4
Allison <abarnett17@gmail.com>
2021-08-05 19:43:30 -0500

    add components

---
+++ src/app/rental-cart/rental-cart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-cart',
+  templateUrl: './rental-cart.component.html',
+  styleUrls: ['./rental-cart.component.css'],
+})
+export class RentalCartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
