25adc5a5c64c9cf6939d232152dfdb107ab1caae
Allison <abarnett17@gmail.com>
2021-12-09 10:01:07 -0600

    images showing, 2 for some reason

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,92 @@ export class AppComponent {
   title = 'flopbuster';
   selectedIndex = -1;
 
-  flops = {
-    hobbit:
-      'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/original/products/86974/86527/the_hobbit_the_desolution_of_smaug__final_poster_buy_original_movie_posters_at_starstills__35272__21743.1394515789.jpg?c=2',
-    newMoon:
-      'https://cdn.shopify.com/s/files/1/1416/8662/products/twilightnewmoon_2009_original_film_art_b4de3022-d6fa-43f4-815a-c2cc4df164f1_1200x.jpg?v=1618949594',
-    velocipastor:
-      'https://m.media-amazon.com/images/M/MV5BZTJiZjFkNmYtNmU3My00MmE1LWI4YWEtNWFkZDJiOTgwMTNkXkEyXkFqcGdeQXVyMTg0MTI3Mg@@._V1_.jpg',
-    cryWilderness:
-      'https://cdn.cinematerial.com/p/297x/knnahuxr/cry-wilderness-movie-poster-md.jpg?v=1456385813',
-  };
+  flops = [
+    {
+      title: 'Battlefield Earth',
+      stars: 2.5,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMTg0Njk2OTM3OF5BMl5BanBnXkFtZTYwNTAyMzc3._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Santa Clause Conquers the Martians',
+      stars: 2.7,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BZDllYzM0YjktYWNjOC00MjZjLWE2Y2EtOGRkMTY1N2I3MjUxXkEyXkFqcGdeQXVyMTQ2MjQyNDc@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'The Room',
+      stars: 3.7,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BYjEzN2FlYmYtNDkwMC00NGFkLWE5ODctYmE5NmYxNzE2MmRiXkEyXkFqcGdeQXVyMjMwODc5Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Howard the Duck',
+      stars: 4.7,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BZWQ5Y2FmMmItZjYzNC00Yzc5LWE0ZjYtMTI4YTA1ZGJkZjgyXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Jack and Jill',
+      stars: 3.3,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BNjczMTU5OTUyMl5BMl5BanBnXkFtZTcwODEzNjc3Ng@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Cats',
+      stars: 2.7,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BNjRlNTY3MTAtOTViMS00ZjE5LTkwZGItMGYwNGQwMjg2NTEwXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Dudley Do-Right',
+      stars: 3.9,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BN2EwZWRhNTQtOWJlMS00NDAwLTk2ZGUtNDg1NjUzZGMyMTYzXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Batman and Robin',
+      stars: 3.7,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMGQ5YTM1NmMtYmIxYy00N2VmLWJhZTYtN2EwYTY3MWFhOTczXkEyXkFqcGdeQXVyNTA2NTI0MTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Catwoman',
+      stars: 3.4,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMjA4MzM0NDAzOF5BMl5BanBnXkFtZTcwMDY3MDYyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Last Airbender',
+      stars: 4.1,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMTM1NjE0NDA0MV5BMl5BanBnXkFtZTcwODE4NDg1Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Spider-Man 3',
+      stars: 6.2,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Transformers: Revenge of the Fallen',
+      stars: 6.0,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BNjk4OTczOTk0NF5BMl5BanBnXkFtZTcwNjQ0NzMzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Twilight',
+      stars: 5.2,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMTQ2NzUxMTAxN15BMl5BanBnXkFtZTcwMzEyMTIwMg@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+    },
+    {
+      title: 'Wing Commander',
+      stars: 4.3,
+      imageUrl:
+        'https://m.media-amazon.com/images/M/MV5BMTc1MDI1MDMyN15BMl5BanBnXkFtZTcwMzgzMTIyMQ@@._V1_UY268_CR1,0,182,268_AL_.jpg',
+    },
+  ];
 
   onVote(index) {
     if (this.isSelected(index)) {
